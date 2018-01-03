4ccdcd6919c106731cc9115bfab07f5f980a098c
Daniel <daniel@elegate.io>
2018-01-03 22:55:50 +0100

    AWSIotMqttClient subclass with connection success callback

---
+++ src/main/kotlin/io/wittekind/mehrnebel/machine/mqtt/MehrNebelMqttClient.kt
@@ -0,0 +1,29 @@
+package io.wittekind.mehrnebel.machine.mqtt
+
+import com.amazonaws.services.iot.client.AWSIotMqttClient
+import org.slf4j.LoggerFactory
+import java.security.KeyStore
+
+internal class MehrNebelMqttClient(endpoint: String, clientId: String, keystorePw: KeyStore, keyPw: String) : AWSIotMqttClient(endpoint, clientId, keystorePw, keyPw) {
+
+    private val logger by lazy {
+        LoggerFactory.getLogger(MehrNebelMqttClient::class.java)
+    }
+
+    private var onConnectionSuccessCallback : () -> Unit = {}
+
+    override fun onConnectionSuccess() {
+        super.onConnectionSuccess()
+        onConnectionSuccessCallback()
+    }
+
+    override fun onConnectionFailure() {
+        super.onConnectionFailure()
+        logger.debug("connection failed: [${connectionStatus.name}]")
+    }
+
+    fun setOnConnectionSuccessCallback(callback : () -> Unit) {
+        onConnectionSuccessCallback = callback
+    }
+
+}
